035fe69de173b635b0898fea7d0675f2393c2a4b
Utkal Kishore Sahoo <utkalkishors@gmail.com>
2022-03-18 00:28:47 +0530

    first push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import react from 'react'
+import logo from './logo.svg'
+import './App.css'
+import TicTacToe from './TicTacToe/TicTacToe'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <TicTacToe />
+    </div>
+  )
+}
+
+export default App
